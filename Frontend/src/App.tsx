400195032ffd522cd30cbe9f84687772f6cd33ba
unknown <shubhamp03080@gmail.com>
2024-04-11 20:12:27 +0530

    hotel detail page

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -7,6 +7,7 @@ import { useAppContext } from "./Contexts/AppContext"
 import MyHotels from "./pages/MyHotels"
 import EditHotel from "./pages/EditHotel"
 import Search from "./pages/Search"
+import Detail from "./pages/Detail"
 
 
 function App() {
@@ -17,6 +18,7 @@ const {isLoggedIn}=useAppContext();
       <Routes>
         <Route path="/" element={<Layout><p>Home</p></Layout>}/>
         <Route path="/search" element={<Layout><Search/></Layout>}/>
+        <Route path="/detail/:hotelId" element={<Layout><Detail/></Layout>}/>
         <Route path="/register" element={<Layout><Register/></Layout>}/>
         <Route path="/sign-in" element={<Layout><SignIn/></Layout>}/>
         {isLoggedIn && (
